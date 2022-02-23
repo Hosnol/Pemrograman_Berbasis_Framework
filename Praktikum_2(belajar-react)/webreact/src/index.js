4809da5ac5e059bfbc2acc6095a733ee14c04a0f
Hosnol Arifin <hosnolarifin220@gmail.com>
2022-02-23 11:11:46 +0700

    Membuat component hello menggunakan arrow function

--- Praktikum_2(belajar-react)/webreact/src/index.js
+++ Praktikum_2(belajar-react)/webreact/src/index.js
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// membuat arrow fuction dengan helloworld
+const Hello = () => {
+  return <h1>Hello, world!</h1>;
+};
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
