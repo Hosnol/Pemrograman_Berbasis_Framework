6018063370a6dae5bbb53559f41b704e930deddd
Hosnol Arifin <hosnolarifin220@gmail.com>
2022-03-09 08:23:55 +0700

    react js api

---
+++ praktikum_4/web-reactku/src/container/BlogPost/BlogPost.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import '././BlogPost.css';
+
+class BlogPost extends Component{
+    render(){
+        return(
+            <p>Blog Article</p>
+        );
+    }
+} export default BlogPost;
