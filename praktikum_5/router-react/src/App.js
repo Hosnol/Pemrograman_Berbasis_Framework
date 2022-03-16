642e54308e9eff4ad741e0e56ecf3f22fd38fe4f
Hosnol Arifin <hosnolarifin220@gmail.com>
2022-03-16 11:30:18 +0700

    redirect Auth

--- praktikum_5/router-react/src/App.js
+++ praktikum_5/router-react/src/App.js
@@ -1,5 +1,14 @@
 import React, { Children } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, useParams, useRouteMatch } from 'react-router-dom';
+import { 
+  BrowserRouter as Router,
+  Route, 
+  Switch, 
+  Link, 
+  useParams, 
+  useRouteMatch, 
+  useLocation, 
+  useHistory, 
+  Redirect } from 'react-router-dom';
 
 // Basic route
 // export default function BasicExample() {
@@ -103,78 +112,185 @@ import { BrowserRouter as Router, Route, Switch, Link, useParams, useRouteMatch 
 // }
 
 // Use Nesting Router
-export default function NestingExample() {
+// export default function NestingExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/topics">Topics</Link>
+//           </li>
+//         </ul>
+//         <hr />
+
+//         <Switch>
+//           <Route exact path="/">
+//             <Home />
+//           </Route>
+//           <Route path="/topics">
+//             <Topics />
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// function Home() {
+//   return (
+//     <div>
+//       <h2>Home</h2>
+//     </div>
+//   );
+// }
+
+// function Topics() {
+//   let {path,url} = useRouteMatch();
+
+//   return (
+//     <div>
+//       <h2>Topics</h2>
+
+//       <ul>
+//         <li>
+//           <Link to={`${url}/Sate, Nasi goreng`}>Kuliner</Link>
+//         </li>
+//         <li>
+//           <Link to={`${url}/Wisata alam, Museum`}>Travelling</Link>
+//         </li>
+//         <li>
+//           <Link to={`${url}/Ibis, Jw Mariot`}>Review Hotel</Link>
+//         </li>
+//       </ul>
+
+//       <Switch>
+//         <Route exact path={path}>
+//           <h3>Please select a topic.</h3>
+//         </Route>
+//         <Route path={`${path}/:topicId`}>
+//           <Topic />
+//         </Route>
+//       </Switch>
+//     </div>
+//   );
+// }
+
+// function Topic(){
+//   let {topicId} = useParams();
+
+//   return (
+//     <div>
+//       <h3>{topicId}</h3>
+//     </div>
+//   );
+// }
+
+// Use redirect Auth
+export default function AuthExample() {
   return (
     <Router>
       <div>
+        <AuthButton />
+
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/public">Public Page</Link>
           </li>
           <li>
-            <Link to="/topics">Topics</Link>
+            <Link to="/private">Private Page</Link>
           </li>
         </ul>
         <hr />
-
+        
         <Switch>
-          <Route exact path="/">
-            <Home />
+          <Route path="/public">
+            <PublicPage />
           </Route>
-          <Route path="/topics">
-            <Topics />
+          <Route path="/login">
+            <LoginPage />
           </Route>
+          <PrivateRoute path="/private">
+            <PrivatePage />
+          </PrivateRoute>
         </Switch>
       </div>
     </Router>
   );
 }
 
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+function AuthButton() {
+  let history = useHistory();
+
+  return fakeAuth.isAuthenticated ? (
+    <p>
+      Welcome!{" "}
+      <button onClick={() => {
+        fakeAuth.signout(() => history.push("/"));
+      }}>Sign out</button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
   );
 }
 
-function Topics() {
-  let {path,url} = useRouteMatch();
-
+function PrivateRoute({ children, ...rest }) {
   return (
-    <div>
-      <h2>Topics</h2>
-
-      <ul>
-        <li>
-          <Link to={`${url}/Sate, Nasi goreng`}>Kuliner</Link>
-        </li>
-        <li>
-          <Link to={`${url}/Wisata alam, Museum`}>Travelling</Link>
-        </li>
-        <li>
-          <Link to={`${url}/Ibis, Jw Mariot`}>Review Hotel</Link>
-        </li>
-      </ul>
-
-      <Switch>
-        <Route exact path={path}>
-          <h3>Please select a topic.</h3>
-        </Route>
-        <Route path={`${path}/:topicId`}>
-          <Topic />
-        </Route>
-      </Switch>
-    </div>
+    <Route
+      {...rest}
+      render={({ location }) =>
+        fakeAuth.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
   );
 }
 
-function Topic(){
-  let {topicId} = useParams();
+function PublicPage() {
+  return <h3>Public</h3>;
+}
+
+function PrivatePage() {
+  return <h3>Private</h3>;
+}
+
+function LoginPage() {
+  let history = useHistory();
+  let location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/" } };
+  let login = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    });
+  };
 
   return (
     <div>
-      <h3>{topicId}</h3>
+      <p>You must log in to view the page at {from.pathname}</p>
+      <button onClick={login}>Log in</button>
     </div>
   );
 }
