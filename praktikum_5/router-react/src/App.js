2a350319148d65ba32c891d9184268ce478d8868
Hosnol Arifin <hosnolarifin220@gmail.com>
2022-03-16 09:40:45 +0700

    URL parameters and Nesting Router

--- praktikum_5/router-react/src/App.js
+++ praktikum_5/router-react/src/App.js
@@ -1,7 +1,109 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import React, { Children } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link, useParams, useRouteMatch } from 'react-router-dom';
 
-export default function BasicExample() {
+// Basic route
+// export default function BasicExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/about">About</Link>
+//           </li>
+//           <li>
+//             <Link to="/dashboard">Dashboard</Link>
+//           </li>
+//         </ul>
+//         <hr />
+        
+//         <Switch>
+//           <Route exact path="/">
+//             <Home/>
+//           </Route>
+//           <Route path="/about">
+//             <About/>
+//           </Route>
+//           <Route path="/dashboard">
+//             <Dashboard/>
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// //Anda dapat mengangap komponen ini sebagai "halaman" yang dapat diakses dari URL.
+
+// function Home() {
+//   return (
+//     <div>
+//       <h2>Home</h2>
+//     </div>
+//   ); 
+// }
+
+// function About() {
+//   return (
+//     <div>
+//       <h2>About</h2>
+//     </div>
+//   );
+// }
+
+// function Dashboard() {
+//   return (
+//     <div>
+//       <h2>Dashboard</h2>
+//     </div>
+//   );
+// }
+
+
+// URL Parameters
+// export default function ParamsExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <h2> Account </h2>
+//         <ul>
+//           <li>
+//             <Link to="/netflix">Netflix</Link>
+//           </li>
+//           <li>
+//             <Link to="/gmail">Gmail</Link>
+//           </li>
+//           <li>
+//             <Link to="/yahoo">Yahoo</Link>
+//           </li>
+//           <li>
+//             <Link to="/amazon">Amazon</Link>
+//           </li>
+//         </ul>
+//         <hr />
+
+//         <Switch>
+//           <Route path="/:id"> {<Child />} </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// function Child() {
+//   let { id } = useParams();
+
+//   return (
+//     <div>
+//       <h3>ID:{id}</h3>
+//     </div>
+//   );
+// }
+
+// Use Nesting Router
+export default function NestingExample() {
   return (
     <Router>
       <div>
@@ -10,23 +112,17 @@ export default function BasicExample() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/topics">Topics</Link>
           </li>
         </ul>
         <hr />
-        
+
         <Switch>
           <Route exact path="/">
-            <Home/>
+            <Home />
           </Route>
-          <Route path="/about">
-            <About/>
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard/>
+          <Route path="/topics">
+            <Topics />
           </Route>
         </Switch>
       </div>
@@ -34,28 +130,51 @@ export default function BasicExample() {
   );
 }
 
-//Anda dapat mengangap komponen ini sebagai "halaman" yang dapat diakses dari URL.
-
 function Home() {
   return (
     <div>
       <h2>Home</h2>
     </div>
-  ); 
+  );
 }
 
-function About() {
+function Topics() {
+  let {path,url} = useRouteMatch();
+
   return (
     <div>
-      <h2>About</h2>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${url}/Sate, Nasi goreng`}>Kuliner</Link>
+        </li>
+        <li>
+          <Link to={`${url}/Wisata alam, Museum`}>Travelling</Link>
+        </li>
+        <li>
+          <Link to={`${url}/Ibis, Jw Mariot`}>Review Hotel</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+        <Route path={`${path}/:topicId`}>
+          <Topic />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-function Dashboard() {
+function Topic(){
+  let {topicId} = useParams();
+
   return (
     <div>
-      <h2>Dashboard</h2>
+      <h3>{topicId}</h3>
     </div>
   );
 }
