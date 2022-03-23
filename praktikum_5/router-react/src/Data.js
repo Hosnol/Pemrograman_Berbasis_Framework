6ba63a49f4669b07e19923461f80a0f1e0e80f92
Hosnol Arifin <hosnolarifin220@gmail.com>
2022-03-23 08:21:52 +0700

    tugas

---
+++ praktikum_5/router-react/src/Data.js
@@ -0,0 +1,59 @@
+const data = {
+    product:[
+        {
+            id: "1",
+            name: "Apple iPhone 8",
+            price: "Rp 3.368.000",
+            image: "./img/Apple iphone 8.jpg"
+        },
+        {
+            id: "2",
+            name: "Apple iPhone 8 Plus",
+            price: "Rp 12.300.000",
+            image: "./img/Apple iphone 8 plus.jpg"
+        },
+        {
+            id: "3",
+            name: "Apple iPhone X",
+            price: "Rp 8.800.000",
+            image: "./img/Apple iphone X.jpg"
+        },
+        {
+            id: "4",
+            name: "Apple iPhone 11 Pro",
+            price: "Rp 12.399.000",
+            image: "./img/Apple iphone 11 pro.jpg"
+        },
+        {
+            id: "5",
+            name: "Apple iPhone 12 Pro Max",
+            price: "Rp 15.390.000",
+            image: "./img/Apple iphone 12 pro max.jpg"
+        },
+        {
+            id: "6",
+            name: "Apple iPhone XR 128GB Putih",
+            price: "Rp 7.650.000",
+            image: "./img/Apple iphone XR.jpg"
+        },
+        {
+            id: "7",
+            name: "Apple iPhone SE 2020",
+            price: "Rp 6.700.000",
+            image: "./img/Apple iphone SE.jpg"
+        },
+        {
+            id: "8",
+            name: "Apple iPhone 11",
+            price: "Rp 10.499.000",
+            image: "./img/Apple iphone 11.jpg"
+        },
+        {
+            id: "9",
+            name: "Apple iPhone 13 Pro Max",
+            price: "Rp 22.600.000",
+            image: "./img/Apple iphone 13 pro.jpg"
+        },
+    ]
+}
+export default data;
