408b2b505c5346db35ee473b7102090f688edd6b
Hosnol Arifin <hosnolarifin220@gmail.com>
2022-03-30 08:13:18 +0700

    todo list

--- praktikum_5/router-react/src/index.js
+++ praktikum_5/router-react/src/index.js
@@ -7,9 +7,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Redirect from './Redirect';
 
-ReactDOM.render(<App />, document.getElementById('root')
+ReactDOM.render(< App/>, document.getElementById('root')
 );
+
+// ReactDOM.render(
+//     <Redirect />,
+//     document.getElementById('root')
+//     );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
