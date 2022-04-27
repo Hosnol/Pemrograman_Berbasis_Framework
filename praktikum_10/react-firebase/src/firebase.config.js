3a333a10c409a8f558c0c7ba1513622b7460982a
Hosnol Arifin <hosnolarifin220@gmail.com>
2022-04-27 15:21:42 +0700

    firebase config

---
+++ praktikum_10/react-firebase/src/firebase.config.js
@@ -0,0 +1,11 @@
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD_4blnzkIViIwelfbV0q775u_yPKmPT04",
+    authDomain: "prak10-fee53.firebaseapp.com",
+    projectId: "prak10-fee53",
+    storageBucket: "prak10-fee53.appspot.com",
+    messagingSenderId: "204078464749",
+    appId: "1:204078464749:web:7882cc8c2b4724c48496e4",
+    measurementId: "G-K4KMF17DRR"
+};
+export default firebaseConfig;
