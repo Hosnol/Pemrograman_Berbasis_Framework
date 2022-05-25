a19a2bf28ec60ef5750c541d50fdbede8b7b1b72
Hosnol Arifin <hosnolarifin220@gmail.com>
2022-05-25 09:05:58 +0700

    config

---
+++ praktikum_12/firebase-crud/src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDbtodmnZkM75OVh3dSZyeO31DcHTHxbdo",
+    authDomain: "praktikum11-22061.firebaseapp.com",
+    databaseURL: "https://praktikum11-22061-default-rtdb.firebaseio.com",
+    projectId: "praktikum11-22061",
+    storageBucket: "praktikum11-22061.appspot.com",
+    messagingSenderId: "467971696268",
+    appId: "1:467971696268:web:963d4d9e14755910967da9",
+    measurementId: "G-N9C9Z12VS2"
+  };
+export default firebaseConfig;
